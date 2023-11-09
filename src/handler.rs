425efc1b025cc9eeb1f0239c466eb0629a6d4366
Dave Jones <sect62@gmail.com>
2023-11-09 09:26:11 -0600

    initial commit

---
+++ src/handler.rs
@@ -0,0 +1,401 @@
+use crate::{Context, Response};
+use hyper::StatusCode;
+use std::collections::HashMap;
+use std::error::Error;
+use std::fmt;
+//use std::fs;
+use serde::{Deserialize, Serialize};
+//use serde_json::{Value};
+use std::time::{SystemTime, UNIX_EPOCH};
+use sha1::{Sha1, Digest};
+use urlencoding;
+use reqwest::header;
+
+
+
+//Globals ----------------------------------------------------------------------------------------------------
+const API_KEY: &str = "B899NK69ERMRE2M6HD3B";
+const API_SECRET: &str = "J3v9m$4b6NCD9ENV4QEKYb^DnWdcGR$^Gq7#5uwS";
+const AP_PUBKEY: &str = "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArYGPVCRtdZXQLAYANU6R\nhH5e5bPQ8ImW7AxOkFRIoAhK0+zJOHsn6UIrpdXK7JcIdkR3pPEG620BVHUkZOVC\nYUsnW7gNWAPyeXMVUPO0h2okCyUIeOSoRuIto8AfsfaQOeLeCIt0bqHymX4FueRi\n6y3fpUlGkNMLJ6T1tfLXElwcNxNnzEV4dvCpwHh9lZwQKersbKFgpVFl5VO9+ZG+\nhO2ym6KMGeD09oPK7lvvhjItfEkqmzOVCkH4PRXaHwcst9lBNwSNKeUkNWfIg8Bd\nqFFCZMx6+VmwBeaFIa8ia9jMT2ofTZ56Whlx7Jo9j7wtTGNeo/HC0v4Uvkbw+o+v\nfQIDAQAB\n-----END PUBLIC KEY-----";
+
+//Structs ----------------------------------------------------------------------------------------------------
+#[allow(non_snake_case)]
+#[derive(Serialize, Deserialize)]
+struct Link {
+    rel: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    r#type: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    href: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    template: Option<String>,
+}
+#[derive(Serialize, Deserialize)]
+struct Webfinger {
+    subject: String,
+    aliases: Vec<String>,
+    links: Vec<Link>,
+}
+
+#[allow(non_snake_case)]
+#[derive(Serialize, Deserialize)]
+struct PublicKey {
+    id: String,
+    owner: String,
+    publicKeyPem: String,
+}
+
+#[allow(non_snake_case)]
+#[derive(Serialize, Deserialize)]
+struct Actor {
+    #[serde(rename="@context")]
+    context: Vec<String>,
+    id: String,
+    r#type: String,
+    preferredUsername: String,
+    inbox: String,
+    publicKey: PublicKey,
+}
+
+#[allow(non_snake_case)]
+#[derive(Serialize, Deserialize)]
+struct PIFeed {
+    id: u64,
+    podcastGuid: String,
+    medium: String,
+    title: String,
+    url: String,
+    originalUrl: String,
+    link: String,
+    description: String,
+    author: String,
+    ownerName: String,
+    image: String,
+    artwork: String,
+    episodeCount: u64,
+
+}
+
+#[allow(non_snake_case)]
+#[derive(Serialize, Deserialize)]
+struct PIPodcast {
+    status: String,
+    feed: PIFeed
+}
+
+#[derive(Debug)]
+struct HydraError(String);
+
+impl fmt::Display for HydraError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Fatal error: {}", self.0)
+    }
+}
+
+impl Error for HydraError {}
+
+
+//Functions --------------------------------------------------------------------------------------------------
+// pub async fn home(ctx: Context) -> Response {
+//     let chat_id: &str;
+//
+//     //Get query parameters
+//     let params: HashMap<String, String> = ctx.req.uri().query().map(|v| {
+//         url::form_urlencoded::parse(v.as_bytes()).into_owned().collect()
+//     }).unwrap_or_else(HashMap::new);
+//
+//     println!("{:#?}", params);
+//
+//     //Make sure a session param was given
+//     match params.get("cid") {
+//         Some(cid) => {
+//             println!("Got a chat session id: {}\n", cid);
+//             chat_id = cid;
+//         }
+//         None => {
+//             println!("Invalid chat session id.\n");
+//             return hyper::Response::builder()
+//                 .status(StatusCode::from_u16(400).unwrap())
+//                 .body(format!("No chat session id given.").into())
+//                 .unwrap();
+//         }
+//     }
+//
+//     //Is the session valid
+//     // match mkultra::check_session(chat_id) {
+//     //     Ok(_) => {
+//     //         let doc = fs::read_to_string("home.html").expect("Something went wrong reading the file.");
+//     //         return hyper::Response::builder()
+//     //             .status(StatusCode::OK)
+//     //             .body(format!("{}", doc).into())
+//     //             .unwrap();
+//     //     }
+//     //     Err(_) => {
+//     //         return hyper::Response::builder()
+//     //             .status(StatusCode::from_u16(400).unwrap())
+//     //             .body(format!("That's not a live session.").into())
+//     //             .unwrap();
+//     //     }
+//     // }
+//
+// }
+
+pub async fn webfinger(ctx: Context) -> Response {
+
+    //Get query parameters
+    let params: HashMap<String, String> = ctx.req.uri().query().map(|v| {
+        url::form_urlencoded::parse(v.as_bytes()).into_owned().collect()
+    }).unwrap_or_else(HashMap::new);
+
+    println!("{:#?}", params);
+
+    //Make sure a session param was given
+    let guid;
+    match params.get("resource") {
+        Some(resource) => {
+            println!("Got a resource: {}\n", resource);
+            let parts = resource.replace("acct:", "");
+            guid = parts.split("@").next().unwrap().to_string();
+        }
+        None => {
+            println!("Invalid resource.\n");
+            return hyper::Response::builder()
+                .status(StatusCode::from_u16(400).unwrap())
+                .body(format!("No resource given.").into())
+                .unwrap();
+        }
+    }
+
+    let podcast_guid = guid.clone();
+
+    //Lookup API of podcast
+    let podcast_data: PIPodcast;
+    let api_response = api_get_podcast(API_KEY, API_SECRET, &podcast_guid).await;
+    match api_response {
+        Ok(response_body) => {
+            //eprintln!("{:#?}", response_body);
+            match serde_json::from_str(response_body.as_str()) {
+                Ok(data) => {
+                    podcast_data = data;
+                    println!("{}", podcast_data.feed.image);
+                }
+                Err(e) => {
+                    println!("Response prep error: [{:#?}].\n", e);
+                    return hyper::Response::builder()
+                        .status(StatusCode::from_u16(501).unwrap())
+                        .body(format!("Response prep error.").into())
+                        .unwrap();
+                }
+            }
+        }
+        Err(e) => {
+            println!("Response prep error: [{:#?}].\n", e);
+            return hyper::Response::builder()
+                .status(StatusCode::from_u16(501).unwrap())
+                .body(format!("Response prep error.").into())
+                .unwrap();
+        }
+    }
+
+
+
+
+
+    //Construct a response
+    let webfinger_data = Webfinger {
+        subject: format!("acct:{}@podcastindex.org", podcast_guid).to_string(),
+        aliases: vec!(
+            format!("https://podcastindex.org/podcast/{}", podcast_guid).to_string()
+        ),
+        links: vec!(
+            Link {
+                rel: "http://webfinger.net/rel/profile-page".to_string(),
+                r#type: Some("text/html".to_string()),
+                href: Some(format!("https://podcastindex.org/podcast/{}", podcast_guid).to_string()),
+                template: None,
+            },
+            Link {
+                rel: "self".to_string(),
+                r#type: Some("application/activity+json".to_string()),
+                href: Some(format!("https://ap.podcastindex.org/podcasts?id={}", podcast_guid).to_string()),
+                template: None,
+            },
+            Link {
+                rel: "http://webfinger.net/rel/avatar".to_string(),
+                r#type: Some("image/jpeg".to_string()),
+                href: Some(format!("{}", podcast_data.feed.image).to_string()),
+                template: None,
+            },
+            Link {
+                rel: "http://ostatus.org/schema/1.0/subscribe".to_string(),
+                r#type: None,
+                href: None,
+                template: Some("https://ap.podcastindex.org/ostatus_subscribe?acct={uri}".to_string()),
+            },
+        ),
+    };
+
+    let webfinger_json;
+    match serde_json::to_string_pretty(&webfinger_data) {
+        Ok(json_result) => {
+            webfinger_json = json_result;
+        }
+        Err(e) => {
+            println!("Response prep error: [{:#?}].\n", e);
+            return hyper::Response::builder()
+                .status(StatusCode::from_u16(500).unwrap())
+                .body(format!("Response prep error.").into())
+                .unwrap();
+        }
+    }
+
+    return hyper::Response::builder()
+        .status(StatusCode::OK)
+        .body(format!("{}", webfinger_json).into())
+        .unwrap();
+
+}
+
+pub async fn actor(ctx: Context) -> Response {
+
+    //Get query parameters
+    let params: HashMap<String, String> = ctx.req.uri().query().map(|v| {
+        url::form_urlencoded::parse(v.as_bytes()).into_owned().collect()
+    }).unwrap_or_else(HashMap::new);
+
+    println!("{:#?}", params);
+
+    //Make sure a session param was given
+    let guid;
+    match params.get("id") {
+        Some(resource) => {
+            println!("Got a resource: {}\n", resource);
+            let parts = resource.replace("acct:", "");
+            guid = parts.split("@").next().unwrap().to_string();
+        }
+        None => {
+            println!("Invalid resource.\n");
+            return hyper::Response::builder()
+                .status(StatusCode::from_u16(400).unwrap())
+                .body(format!("No resource given.").into())
+                .unwrap();
+        }
+    }
+
+    let podcast_guid = guid.clone();
+
+    //Lookup API of podcast
+    let podcast_data: PIPodcast;
+    let api_response = api_get_podcast(API_KEY, API_SECRET, &podcast_guid).await;
+    match api_response {
+        Ok(response_body) => {
+            //eprintln!("{:#?}", response_body);
+            match serde_json::from_str(response_body.as_str()) {
+                Ok(data) => {
+                    podcast_data = data;
+                    println!("{}", podcast_data.feed.image);
+                }
+                Err(e) => {
+                    println!("Response prep error: [{:#?}].\n", e);
+                    return hyper::Response::builder()
+                        .status(StatusCode::from_u16(501).unwrap())
+                        .body(format!("Response prep error.").into())
+                        .unwrap();
+                }
+            }
+        }
+        Err(e) => {
+            println!("Response prep error: [{:#?}].\n", e);
+            return hyper::Response::builder()
+                .status(StatusCode::from_u16(501).unwrap())
+                .body(format!("Response prep error.").into())
+                .unwrap();
+        }
+    }
+
+    //Construct a response
+    let actor_data = Actor {
+        context: vec!(
+            "https://www.w3.org/ns/activitystreams".to_string(),
+            "https://w3id.org/security/v1".to_string(),
+        ),
+        id: format!("https://ap.podcastindex.org/podcasts?id={}", podcast_guid).to_string(),
+        r#type: "Person".to_string(),
+        preferredUsername: podcast_guid.clone(),
+        inbox: format!("https://ap.podcastindex.org/podcasts?id={}&resource=inbox", podcast_guid).to_string(),
+        publicKey: PublicKey {
+            id: format!("https://ap.podcastindex.org/podcasts?id={}#main-key", podcast_guid).to_string(),
+            owner: format!("https://ap.podcastindex.org/podcasts?id={}", podcast_guid).to_string(),
+            publicKeyPem: AP_PUBKEY.to_string(),
+        }
+    };
+
+    let actor_json;
+    match serde_json::to_string_pretty(&actor_data) {
+        Ok(json_result) => {
+            actor_json = json_result;
+        }
+        Err(e) => {
+            println!("Response prep error: [{:#?}].\n", e);
+            return hyper::Response::builder()
+                .status(StatusCode::from_u16(500).unwrap())
+                .body(format!("Response prep error.").into())
+                .unwrap();
+        }
+    }
+
+    return hyper::Response::builder()
+        .status(StatusCode::OK)
+        .body(format!("{}", actor_json).into())
+        .unwrap();
+
+}
+
+pub async fn api_get_podcast(key: &'static str, secret: &'static str, query: &str) -> Result<String, Box<dyn Error>> {
+    println!("Running...");
+
+    let api_key = key;
+    let api_secret = secret;
+
+    //##: ======== Required values ========
+    //##: WARNING: don't publish these to public repositories or in public places!
+    //##: NOTE: values below are sample values, to get your own values go to https://api.podcastindex.org
+    let api_time = SystemTime::now().duration_since(UNIX_EPOCH).expect("Time mismatch.").as_secs().to_string();
+
+    //##: Create the authorization token.
+    //##: The auth token is built by creating an sha1 hash of the key, secret and current time (as a string)
+    //##: concatenated together. The hash is a lowercase string.
+    let data4hash: String = format!("{}{}{}", api_key, api_secret, api_time);
+    println!("Data to hash: [{}]", data4hash);
+    let mut hasher = Sha1::new();
+    hasher.update(data4hash);
+    let authorization_token = hasher.finalize();
+    let api_hash: String = format!("{:X}", authorization_token).to_lowercase();
+    println!("Hash String: [{}]", api_hash);
+
+    //##: Set up the parameters and the api endpoint url to call and make sure all params are
+    //##: url encoded before sending.
+    let url: String = format!("https://api.podcastindex.org/api/1.0/podcasts/byfeedid?id={}", urlencoding::encode(query));
+
+    //##: Build the query with the required headers
+    let mut headers = header::HeaderMap::new();
+    headers.insert("User-Agent", header::HeaderValue::from_static("Rust-podcastindex-org-example/v1.0"));
+    headers.insert("X-Auth-Date", header::HeaderValue::from_str(api_time.as_str()).unwrap());
+    headers.insert("X-Auth-Key", header::HeaderValue::from_static(api_key));
+    headers.insert("Authorization", header::HeaderValue::from_str(api_hash.as_str()).unwrap());
+    let client = reqwest::Client::builder().default_headers(headers).build().unwrap();
+
+    //##: Send the request and display the results or the error
+    let res = client.get(url.as_str()).send();
+    match res.await {
+        Ok(res) => {
+            println!("Response Status: [{}]", res.status());
+            return Ok(res.text().await.unwrap())
+        },
+        Err(e) => {
+            eprintln!("API response error: [{}]", e);
+            return Err(Box::new(HydraError(format!("Error running SQL query: [{}]", e).into())));
+        }
+    }
+
+}
---
+++ src/main.rs
@@ -0,0 +1,140 @@
+use hyper::{
+    body::to_bytes,
+    service::{make_service_fn, service_fn},
+    Body, Request, Server,
+};
+use route_recognizer::Params;
+use router::Router;
+use std::sync::Arc;
+use hyper::server::conn::AddrStream;
+//use std::thread;
+//use std::time;
+//use tokio::task;
+use std::env;
+//use drop_root::set_user_group;
+
+//Globals ----------------------------------------------------------------------------------------------------
+//const ZMQ_SOCKET_ADDR: &str = "tcp://127.0.0.1:5555";
+
+mod handler;
+mod router;
+
+type Response = hyper::Response<hyper::Body>;
+type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
+
+
+//Structs ----------------------------------------------------------------------------------------------------
+#[derive(Clone, Debug)]
+pub struct AppState {
+    pub state_thing: String,
+    pub remote_ip: String,
+}
+
+#[derive(Debug)]
+pub struct Context {
+    pub state: AppState,
+    pub req: Request<Body>,
+    pub params: Params,
+    body_bytes: Option<hyper::body::Bytes>,
+}
+
+//Functions --------------------------------------------------------------------------------------------------
+#[tokio::main]
+async fn main() {
+
+    let args: Vec<String> = env::args().collect();
+    let arg_port = &args[1];
+    let arg_chatid = &args[2];
+
+    //TODO: these must handle errors better
+    //Validate the passed in chat id and clear anything else from the db
+    // if mkultra::init_database().is_err() {
+    //     eprintln!("Error initializing the database file.");
+    // }
+    // if mkultra::init_chat_session_with_id(arg_chatid).is_err() {
+    //     eprintln!("Error initializing chat: [{}] in the database.", arg_chatid);
+    // }
+    println!("Initialized session: [{}]", arg_chatid);
+
+    let some_state = "state".to_string();
+
+    let mut router: Router = Router::new();
+    router.get("/podcasts", Box::new(handler::actor));
+    router.get("/.well-known/webfinger", Box::new(handler::webfinger));
+
+    let shared_router = Arc::new(router);
+    let new_service = make_service_fn(move |conn: &AddrStream| {
+        let app_state = AppState {
+            state_thing: some_state.clone(),
+            remote_ip: conn.remote_addr().to_string().clone(),
+        };
+
+        let router_capture = shared_router.clone();
+        async {
+            Ok::<_, Error>(service_fn(move |req| {
+                route(router_capture.clone(), req, app_state.clone())
+            }))
+        }
+    });
+
+    let binding = format!("0.0.0.0:{}", arg_port);
+    let addr = binding.parse().expect("address creation works");
+    let server = Server::bind(&addr).serve(new_service);
+    println!("Listening on http://{}", addr);
+
+    //If a "run as" user is set in the "PODPING_RUN_AS" environment variable, then switch to that user
+    //and drop root privileges after we've bound to the low range socket
+    // match env::var("PODPING_RUNAS_USER") {
+    //     Ok(runas_user) => {
+    //         match set_user_group(runas_user.as_str(), "nogroup") {
+    //             Ok(_) => {
+    //                 println!("RunAs: {}", runas_user.as_str());
+    //             }
+    //             Err(e) => {
+    //                 eprintln!("RunAs Error: {} - Check that your PODPING_RUNAS_USER env var is set correctly.", e);
+    //             }
+    //         }
+    //     }
+    //     Err(_) => {
+    //         eprintln!("ALERT: Use the PODPING_RUNAS_USER env var to avoid running as root.");
+    //     }
+    // }
+
+    let _ = server.await;
+}
+
+async fn route(
+    router: Arc<Router>,
+    req: Request<hyper::Body>,
+    app_state: AppState,
+) -> Result<Response, Error> {
+    let found_handler = router.route(req.uri().path(), req.method());
+    let resp = found_handler
+        .handler
+        .invoke(Context::new(app_state, req, found_handler.params))
+        .await;
+    Ok(resp)
+}
+
+impl Context {
+    pub fn new(state: AppState, req: Request<Body>, params: Params) -> Context {
+        Context {
+            state,
+            req,
+            params,
+            body_bytes: None,
+        }
+    }
+
+    pub async fn body_json<T: serde::de::DeserializeOwned>(&mut self) -> Result<T, Error> {
+        let body_bytes = match self.body_bytes {
+            Some(ref v) => v,
+            _ => {
+                let body = to_bytes(self.req.body_mut()).await?;
+                self.body_bytes = Some(body);
+                self.body_bytes.as_ref().expect("body_bytes was set above")
+            }
+        };
+        Ok(serde_json::from_slice(&body_bytes)?)
+    }
+}
